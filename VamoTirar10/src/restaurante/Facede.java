2de61eb60ad9fa075bbfbabd55e339c855bfb994
nunesdavidson <sentidossentidos@hotmail.com>
2016-12-10 11:15:47 -0200

    Implementação da facede
    Agradeceria se começassemos a implementar o codigo fonte.

---
+++ VamoTirar10/src/restaurante/Facede.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurante;
+
+/**
+ *
+ * @author Davidson
+ */
+abstract class Facede {
+    //calcula o troco na venda de tickets e seta o livro caixa, retorna o troco.
+    abstract float calculoTroco(int qtdTicket, float valorTicket, float valorPago,float troco);
+    //imprime os tickets no arquivo selecionado.
+    abstract void imprimeTicket(int qtdImpressões);
+    //recebe os tickets dos clientes, seta o ticket como usado no banco, retorna se ticket verdadeiro ou falso.
+    abstract boolean recebeTicket(long nTicket);
+    //cadastro de clientes, seta o cadastro, retorna se fez o cadastro corretamente.
+    abstract boolean recebeCadastro(String nome, int identificador);
+    //calendário
+    
+    //relatório geral, imprime a tabela na tela RelatorioGeral;
+    abstract void relatorioGeral();
+    //relatório cliente, imprime a tabela na tela Cliente;
+    abstract void relatotioCliente(String tipo, int identificador);
+    //criar funcionário do tipo gerente (pode tudo) ou do tipo funcionário do ru (pode vender e receber)
+    abstract void criaFuncionario(String tipo, String nome, String login, String senha);
+    
+    
+    
+    
+}
